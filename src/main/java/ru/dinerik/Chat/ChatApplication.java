19467f92c52bb3dc9619119b3a7984f59793d0b0
DJ-UKOL <dj-ukol@yandex.ru>
2023-03-28 09:27:36 +0500

    Start project Chat

---
+++ src/main/java/ru/dinerik/Chat/ChatApplication.java
@@ -0,0 +1,13 @@
+package ru.dinerik.Chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatApplication.class, args);
+	}
+
+}
